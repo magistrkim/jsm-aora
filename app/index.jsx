45cba7aa8f9a3388a2553dfb968ceab36401ad59
Nataliya Kim <magistr1982@gmail.com>
2024-05-12 10:58:17 +0300

    Add fonts to project

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 bg-white justify-center items-center">
-      <Text className="text-blue-800 text-3xl">Welcome to Aora!</Text>
+      <Text className="text-blue-800 text-3xl font-pblack">Welcome to Aora!</Text>
       <StatusBar style="auto" />
     </View>
   );
