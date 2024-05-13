13d1b94a43902a84f81cd555e655e423f8a277ee
Nataliya Kim <magistr1982@gmail.com>
2024-05-13 09:38:31 +0300

    Create Bottom Tabs

--- app/index.jsx
+++ app/index.jsx
@@ -5,10 +5,13 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 bg-white justify-center items-center">
-          <Text className="text-blue-800 text-3xl font-pblack">Welcome to Aora!</Text>
-          <Link href="/profile" style={{color: "red"}}>Go to Profile</Link>
+      <Text className="text-blue-800 text-3xl font-pblack">
+        Welcome to Aora!
+      </Text>
+      <Link href="/home" style={{ color: "red" }}>
+        Go to HomePage
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
 }
-
