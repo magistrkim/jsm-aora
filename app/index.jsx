43c28bd249287d483958d94be8e02478bd9a0556
Nataliya Kim <magistr1982@gmail.com>
2024-05-13 08:59:38 +0300

    Create folder and route structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View className="flex-1 bg-white justify-center items-center">
-      <Text className="text-blue-800 text-3xl font-pblack">Welcome to Aora!</Text>
+          <Text className="text-blue-800 text-3xl font-pblack">Welcome to Aora!</Text>
+          <Link href="/profile" style={{color: "red"}}>Go to Profile</Link>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#18443e",
+        primary: "#153d37",
         secondary: {
           DEFAULT: "#FF9C01",
           100: "#FF9001",
