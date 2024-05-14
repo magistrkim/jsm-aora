67cbf5ad294f753ba7543959c0734375b520c592
Nataliya Kim <magistr1982@gmail.com>
2024-05-14 12:25:39 +0300

    Create Onboarding Screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white justify-center items-center">
-      <Text className="text-blue-800 text-3xl font-pblack">
-        Welcome to Aora!
-      </Text>
-      <Link href="/home" style={{ color: "red" }}>
-        Go to HomePage
-      </Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="h-full bg-primary">
+          <ScrollView contentContainerStyle={{height: "100%"}}>
+              <View className="w-full h-full justify-center items-center px-4">
+                  <Image source={images.logo} resizeMode="contain" className="w-[130px] h-[84px]"/>
+                  <Image source={images.cards} resizeMode="contain" className="max-w-[380px] w-full h-[300px]"/>
+              </View>
+        </ScrollView>
+    </SafeAreaView>
   );
 }
