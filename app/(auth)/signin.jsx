4c6670d0b3fdad43457d98678ca2e13c6390c06c
Nataliya Kim <magistr1982@gmail.com>
2024-05-16 11:10:27 +0300

    Add KeyboardAvoidingView

--- app/(auth)/signin.jsx
+++ app/(auth)/signin.jsx
@@ -1,4 +1,13 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  TouchableWithoutFeedback,
+} from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
@@ -14,50 +23,57 @@ const SignIn = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const submitForm = () => {};
   return (
-    <SafeAreaView className="bg-primary h-full">
-      <ScrollView>
-        <View className="w-full h-[80vh] justify-center px-4 my-6">
-          <Image
-            source={images.logo}
-            resizeMode="contain"
-            className="w-[115px] h-[35px]"
-          />
-          <Text className="text-white text-2xl font-psemibold mt-10 text-semibold">
-            Sign in to Aora
-          </Text>
-          <FormField
-            title="E-mail"
-            value={form.email}
-            handleChangeText={(event) => setForm({ ...form, email: event })}
-            otherStyles="mt-7"
-            keyboardType="email-address"
-          />
-          <FormField
-            title="Password"
-            value={form.password}
-            handleChangeText={(event) => setForm({ ...form, password: event })}
-            otherStyles="mt-7"
-          />
-          <CustomButton
-            title="Sign In"
-            handlePress={submitForm}
-            containerStyles="mt-7"
-            isLoading={isSubmitting}
-          />
-          <View className="justify-center gap-2 flex-row pt-5">
-            <Text className="text-gray-100 text-lg font-pregular">
-              Do not have an account yet?
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      className="flex-1"
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SafeAreaView className="bg-primary h-full">
+          <View className="w-full h-full justify-center px-4 my-6 flex-1">
+            <Image
+              source={images.logo}
+              resizeMode="contain"
+              className="w-[115px] h-[35px]"
+            />
+            <Text className="text-white text-2xl font-psemibold mt-10 text-semibold">
+              Sign in to Aora
             </Text>
-            <Link
-              href="/signup"
-              className="text-secondary text-lg font-psemibold"
-            >
-              Sign Up
-            </Link>
+            <FormField
+              title="E-mail"
+              value={form.email}
+              handleChangeText={(event) => setForm({ ...form, email: event })}
+              otherStyles="mt-7"
+              keyboardType="email-address"
+            />
+            <FormField
+              title="Password"
+              value={form.password}
+              handleChangeText={(event) =>
+                setForm({ ...form, password: event })
+              }
+              otherStyles="mt-7"
+            />
+            <CustomButton
+              title="Sign In"
+              handlePress={submitForm}
+              containerStyles="mt-7"
+              isLoading={isSubmitting}
+            />
+            <View className="justify-center gap-2 flex-row pt-5">
+              <Text className="text-gray-100 text-lg font-pregular">
+                Do not have an account yet?
+              </Text>
+              <Link
+                href="/signup"
+                className="text-secondary text-lg font-psemibold"
+              >
+                Sign Up
+              </Link>
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
