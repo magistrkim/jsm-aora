48bdabc6f5016d11d5348d186e5b0a9a64cfde48
Nataliya Kim <magistr1982@gmail.com>
2024-05-21 05:42:37 +0300

    Complete SearchInput

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,10 +1,31 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { icons } from "../constants";
 
-const SearchInput = () => {
+const SearchInput = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  //   const [showPassword, setShowPassword] = useState(false);
   return (
-    <View>
-      <Text className="font-psemibold text-2xl text-white">SearchInput</Text>
+    <View
+      className="border-2 border-[#0c2320] w-full h-16 bg-[#0f2a26] px-4 
+              rounded-xl focus:border-secondary items-center flex-row space-x-4"
+    >
+      <TextInput
+        className="text-white text-base font-pregular flex-1 mt-0.5"
+        value={value}
+        placeholder={placeholder}
+        placeholderTextColor="#7b7b8b"
+        onChangeText={handleChangeText}
+      />
+      <TouchableOpacity onPress={() => {}}>
+        <Image source={icons.search} className="w-6 h-6" resizeMode="contain" />
+      </TouchableOpacity>
     </View>
   );
 };
