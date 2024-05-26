711c432551cd31cdf1392bf811b0baed4eced25d
Nataliya Kim <magistr1982@gmail.com>
2024-05-26 10:39:13 +0300

    Create TrendingItem with animation

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,14 +1,84 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from "react-native";
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+import { icons } from "../constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Text className="font-psemibold text-sm text-white">
+          Video is playing
+        </Text>
+      ) : (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+          className="relative justify-center items-center"
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 my-5 rounded-[20px] overflow-hidden shadow-2xl shadow-white"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
+  const viewableItemsChanged = ({ viewableItems }) => {
+    setActiveItem(viewableItems[0].key);
+  };
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="font-psemibold text-2xl text-white">{item.id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{ x: 170 }}
       horizontal
     />
   );
