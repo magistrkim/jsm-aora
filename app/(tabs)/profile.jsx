2d2f840f146bf1a435c153c717a4b40a7b91cac3
Nataliya Kim <magistr1982@gmail.com>
2024-05-28 13:36:51 +0300

    Complete signOut in Profile Page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -4,16 +4,21 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import EmptyState from "../../components/EmptyState";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
-import { getUserPosts } from "./../../lib/appwrite";
+import { getUserPosts, signOut } from "./../../lib/appwrite";
 import { useGlobalContext } from "./../../context/GlobalProvider";
 import { icons } from "../../constants";
 import InfoBox from "../../components/InfoBox";
+import { router } from "expo-router";
 
 const Profile = () => {
-  const { user, setUser, isLoggedIn } = useGlobalContext();
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
-
-  const logout = () => {};
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
+    router.replace("/signin");
+  };
 
   return (
     <SafeAreaView className="h-full bg-primary">
