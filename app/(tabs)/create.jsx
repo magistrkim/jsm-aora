284115210ca13585b7f3f512b36d75dc27b35965
Nataliya Kim <magistr1982@gmail.com>
2024-05-29 12:39:54 +0300

    Add upload video to Create Page

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,14 +1,55 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import FormField from './../../components/FormField';
+import FormField from "./../../components/FormField";
+import { Video, ResizeMode } from "expo-av";
+import { icons } from "../../constants";
 
 const Create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
   return (
     <SafeAreaView className="h-full bg-primary">
       <ScrollView className="px-4 my-6">
         <Text className="font-psemibold text-2xl text-white">Upload Video</Text>
-        <FormField/>
+        <FormField
+          title="Video Title"
+          value={form.title}
+          placeholder="Give your video a catch title..."
+          handleChangeText={(event) => setForm({ ...form, title: event })}
+          otherStyles="mt-10"
+        />
+        <View className="mt-7 space-y-2">
+          <Text className="font-pmedium text-base text-gray-100">
+            Upload Video
+          </Text>
+          <TouchableOpacity>
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                className="w-full h-64 rounded-xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="bg-[#0f2a26] w-full h-40 rounded-xl justify-center items-center px-4 border-2 border-[#0c2320]">
+                <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center rounded-lg">
+                  <Image
+                    source={icons.upload}
+                    resizeMode="contain"
+                    className="w-1/2 h-1/2"
+                  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
