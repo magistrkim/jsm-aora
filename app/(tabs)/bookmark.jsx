249f4b21fd50a85afee5bd92d9ca1c8527b48d94
Nataliya Kim <magistr1982@gmail.com>
2024-06-14 13:47:40 +0300

    Add bookmark UI

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, FlatList } from "react-native";
+import EmptyState from "../../components/EmptyState";
+import SearchInput from "../../components/SearchInput";
+import VideoCard from "../../components/VideoCard";
+import React from "react";
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="h-full bg-primary">
+      <FlatList
+        // data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-psemibold text-2xl text-white">
+              Saved Videos
+            </Text>
+            <View className="mt-6 mb-8">
+              <SearchInput
+              // initialQuery={query}
+              />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Bookmark
+export default Bookmark;
